122d87f8c5b81c6560858131a5790adbc39aad42
maxbayne <maxbayne@gmail.com>
2026-03-25 22:22:44 +0200

    install combox component

--- src/components/pages/ShadCNComponent.jsx
+++ src/components/pages/ShadCNComponent.jsx
@@ -9,18 +9,39 @@ import { ArrowUpIcon } from "lucide-react"
 
 //import shad CN ui components
 import { Button } from "@/components/ui/button"
+import { Combobox, ComboboxContent, ComboboxEmpty, ComboboxInput, ComboboxItem, ComboboxList,} from "@/components/ui/combobox"
 
 
 
 export default function ShadCNComponent() 
 {
  
+  const frameworks = ["Next.js", "SvelteKit", "Nuxt.js", "Remix", "Astro"]
+
 
   return (
     <>
       <Button variant="outline">
         <ArrowUpIcon /> Click me
       </Button>
+
+
+      <br/>
+      <br/>
+
+      <Combobox items={frameworks}>
+      <ComboboxInput placeholder="Select a framework" />
+      <ComboboxContent>
+        <ComboboxEmpty>No items found.</ComboboxEmpty>
+        <ComboboxList>
+          {(item) => (
+            <ComboboxItem key={item} value={item}>
+              {item}
+            </ComboboxItem>
+          )}
+        </ComboboxList>
+      </ComboboxContent>
+    </Combobox>
 
 
     </>
